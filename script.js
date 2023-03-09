0988dc31416d4d5e30d434f43ebc6c226b816b9f
Tim <tim.spiele1@freenet.de>
2023-03-09 20:46:26 +0100

    update progressbar

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ let currentPokemon;
 
 
 async function loadPokemon() {
-    for (i = 1; i < 50; i++) {
+    for (i = 1; i < 1009; i++) {
         let url = `https://pokeapi.co/api/v2/pokemon/${i}/`
         let response = await fetch(url);
         let currentPokemon = await response.json();
         printPokemon(currentPokemon);
+        refreshLoadingBar(i)
+        console.log()
     }
+}
+
+function refreshLoadingBar(l) {
+   let loadingBar = (l / 1009) * 100
+   document.getElementById('loadingBar').style = `width:${loadingBar}%`
+   document.getElementById('loadingBar').innerText = `Pokemon are loading: ${loadingBar.toFixed()}%`
+   if(loadingBar.toFixed() > 98){
+    document.getElementById('loadingBar').innerText = 'alle Pokemon geladen. Viel Spaß :)'
+   }
+   console.log(`${loadingBar}%`)
 }
 
 function printPokemon(cP) {
